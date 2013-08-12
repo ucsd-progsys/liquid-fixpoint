e6db411162e126fbbfc509312494fcd9ca256b09
Ranjit Jhala <jhala@cs.ucsd.edu>
2013-08-12 16:04:19 -0700

    misc edits to ticker

--- external/misc/fixMisc.ml
+++ external/misc/fixMisc.ml
@@ -925,12 +925,19 @@ let display_tick = fun () -> print_now "."
 
 let display_tick = 
   let icona = [| "|"; "/" ; "-"; "\\" |] in
-  let pos   = ref 0 in
+  let n     = ref 0                      in
+  let pos   = ref 0                      in
   fun () -> 
-    let k = !pos in
-    let _ = print_now ("\b."^icona.(k)) in
-    let _ = pos := (k + 1) mod 4 in
+    let k   = !pos                       in
+    let _   = pos := (k + 1) mod 4       in
+    let _   = incr n                     in
+    let suf = if (!n mod 76) = 0 
+              then "\n" 
+              else icona.(k)             in
+    let _   = print_now ("\b."^suf)      in
     ()
+
+
 
 let with_out_file file f = file |> open_out >> f |> close_out
 
