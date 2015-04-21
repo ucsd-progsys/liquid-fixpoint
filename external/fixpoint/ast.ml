ecb31b9156f8a5f5eff455ed6e9e6180cb233373
Ranjit Jhala <jhala@cs.ucsd.edu>
2015-04-21 13:43:00 -0700

    ads

--- external/fixpoint/ast.ml
+++ external/fixpoint/ast.ml
@@ -1368,7 +1368,8 @@ let sortcheck_app g f tExp uf es =
         if Sort.check_arity n s then
            Some (s, t)
         else
-    (* None *)
+          None 
+          (*
           let msg = Printf.sprintf  "Ast.sortcheck_app: type params not instantiated %s: n = %d, s = %s, t = %s, tExp = %s"
                       (expr_to_string (eApp (uf, es)))
                       n
@@ -1377,6 +1378,7 @@ let sortcheck_app g f tExp uf es =
                       (opt_to_string Sort.to_string tExp)
           in
              assertf "%s" msg
+          *)
     | _ -> None
 
 
