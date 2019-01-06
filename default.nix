eae500f043f3ed6accf05519bacc6d99619837fe
Amy de Buitléir <amy@nualeargais.ie>
2019-01-06 11:55:03 +0000

    Update to build under NixOS+GHC 8.4.4

--- default.nix
+++ default.nix
@@ -1,31 +1,34 @@
-{ fetchgitLocal }:
 { mkDerivation, ansi-terminal, array, ascii-progress, async
 , attoparsec, base, bifunctors, binary, boxes, bytestring, cereal
 , cmdargs, containers, deepseq, directory, dotgen, fgl
-, fgl-visualize, filemanip, filepath, ghc-prim, hashable
-, intern, located-base, mtl, parallel
-, parsec, pretty, process, stdenv, syb, tasty
-, tasty-hunit, tasty-rerun, text, text-format, transformers
+, fgl-visualize, filemanip, filepath, ghc-prim, git, hashable
+, intern, located-base, mtl, nettools, ocaml, parallel, parallel-io
+, parsec, pretty, process, stdenv, stm, syb, tasty, tasty-ant-xml
+, tasty-hunit, tasty-rerun, text, text-format, time, transformers
 , unordered-containers, z3
 }:
 mkDerivation {
   pname = "liquid-fixpoint";
   version = "0.8.0.1";
-  src = fetchgitLocal ./.;
+  src = ./.;
+  configureFlags = [ "-fbuild-external" ];
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
     ansi-terminal array ascii-progress async attoparsec base bifunctors
     binary boxes bytestring cereal cmdargs containers deepseq directory
     dotgen fgl fgl-visualize filemanip filepath ghc-prim hashable
-    intern located-base mtl parallel parsec pretty process
-    syb text text-format transformers unordered-containers
+    intern located-base mtl parallel parallel-io parsec pretty process
+    syb text text-format time transformers unordered-containers
   ];
   executableHaskellDepends = [ base ];
+  executableSystemDepends = [ ocaml ];
   testHaskellDepends = [
-    base directory filepath process tasty tasty-hunit tasty-rerun text
+    base containers directory filepath mtl process stm tasty
+    tasty-ant-xml tasty-hunit tasty-rerun text transformers
   ];
-  testSystemDepends = [ z3 ];
+  testSystemDepends = [ git nettools z3 ];
+  doCheck = false;
   homepage = "https://github.com/ucsd-progsys/liquid-fixpoint";
   description = "Predicate Abstraction-based Horn-Clause/Implication Constraint Solver";
   license = stdenv.lib.licenses.bsd3;
--- shell.nix
+++ shell.nix
@@ -1,16 +1,52 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default", doBenchmark ? false }:
 
 let
 
   inherit (nixpkgs) pkgs;
 
-  f = import ./default.nix { inherit (pkgs) fetchgitLocal; };
+  f = { mkDerivation, ansi-terminal, array, ascii-progress, async
+      , attoparsec, base, bifunctors, binary, boxes, bytestring, cereal
+      , cmdargs, containers, deepseq, directory, dotgen, fgl
+      , fgl-visualize, filemanip, filepath, ghc-prim, git, hashable
+      , intern, located-base, mtl, nettools, ocaml, parallel, parallel-io
+      , parsec, pretty, process, stdenv, stm, syb, tasty, tasty-ant-xml
+      , tasty-hunit, tasty-rerun, text, text-format, time, transformers
+      , unordered-containers, z3
+      }:
+      mkDerivation {
+        pname = "liquid-fixpoint";
+        version = "0.8.0.1";
+        src = ./.;
+        configureFlags = [ "-fbuild-external" ];
+        isLibrary = true;
+        isExecutable = true;
+        libraryHaskellDepends = [
+          ansi-terminal array ascii-progress async attoparsec base bifunctors
+          binary boxes bytestring cereal cmdargs containers deepseq directory
+          dotgen fgl fgl-visualize filemanip filepath ghc-prim hashable
+          intern located-base mtl parallel parallel-io parsec pretty process
+          syb text text-format time transformers unordered-containers
+        ];
+        executableHaskellDepends = [ base ];
+        executableSystemDepends = [ ocaml ];
+        testHaskellDepends = [
+          base containers directory filepath mtl process stm tasty
+          tasty-ant-xml tasty-hunit tasty-rerun text transformers
+        ];
+        testSystemDepends = [ git nettools z3 ];
+        doCheck = false;
+        homepage = "https://github.com/ucsd-progsys/liquid-fixpoint";
+        description = "Predicate Abstraction-based Horn-Clause/Implication Constraint Solver";
+        license = stdenv.lib.licenses.bsd3;
+      };
 
   haskellPackages = if compiler == "default"
                        then pkgs.haskellPackages
                        else pkgs.haskell.packages.${compiler};
 
-  drv = haskellPackages.callPackage f { inherit (pkgs) z3; };
+  variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
+
+  drv = variant (haskellPackages.callPackage f {});
 
 in
 
