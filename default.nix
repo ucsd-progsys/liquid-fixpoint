9582624ba361c73509397771fa3bb578532daebd
Amy de Buitléir <amy@nualeargais.ie>
2019-01-06 11:34:42 +0000

    alphabetise packages

--- default.nix
+++ default.nix
@@ -1,25 +1,25 @@
 { fetchgitLocal }:
 { mkDerivation, ansi-terminal, array, ascii-progress, async
 , attoparsec, base, bifunctors, binary, boxes, bytestring, cereal
-, cmdargs, containers, deepseq, directory, filemanip, filepath
-, ghc-prim, hashable, intern, located-base, mtl, parallel, parsec, pretty
-, process, stdenv, syb, tasty, tasty-hunit, tasty-rerun, text
-, text-format, transformers, unordered-containers, z3
-, dotgen, fgl, fgl-visualize
+, cmdargs, containers, deepseq, directory, dotgen, fgl
+, fgl-visualize, filemanip, filepath, ghc-prim, hashable
+, intern, located-base, mtl, parallel
+, parsec, pretty, process, stdenv, syb, tasty
+, tasty-hunit, tasty-rerun, text, text-format, transformers
+, unordered-containers, z3
 }:
 mkDerivation {
   pname = "liquid-fixpoint";
-  version = "9.9.9.9";
+  version = "0.8.0.1";
   src = fetchgitLocal ./.;
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
     ansi-terminal array ascii-progress async attoparsec base bifunctors
     binary boxes bytestring cereal cmdargs containers deepseq directory
-    filemanip filepath ghc-prim hashable intern located-base mtl parallel parsec
-    pretty process syb text text-format transformers
-    unordered-containers
-    dotgen fgl fgl-visualize
+    dotgen fgl fgl-visualize filemanip filepath ghc-prim hashable
+    intern located-base mtl parallel parsec pretty process
+    syb text text-format transformers unordered-containers
   ];
   executableHaskellDepends = [ base ];
   testHaskellDepends = [
