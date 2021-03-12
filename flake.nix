f6a4c022652a860f9c0549d7c3b4a854cbc2bc4d
PLR <51248199+plredmond@users.noreply.github.com>
2021-03-12 14:09:36 -0800

    only override a single ghc package set

--- flake.nix
+++ flake.nix
@@ -10,15 +10,20 @@
   outputs = { self, nixpkgs, flake-utils }:
     let
       composeOverlays = funs: builtins.foldl' nixpkgs.lib.composeExtensions (self: super: { }) funs;
-      haskellPackagesOverlay = final: prev: overrides: {
-        haskell = prev.haskell // { packageOverrides = prev.lib.composeExtensions overrides prev.haskell.packageOverrides; };
+      haskellPackagesOverlay = compiler: final: prev: overrides: {
+        haskell = prev.haskell // {
+          packages = prev.haskell.packages // {
+            ${compiler} = prev.haskell.packages.${compiler}.extend overrides;
+          };
+        };
       };
+      ghc = "ghc8102"; # TODO: make this an input?
       mkOutputs = system: {
 
         defaultPackage = (import nixpkgs {
           inherit system;
           overlays = [ self.overlay.${system} ];
-        }).haskellPackages.liquid-fixpoint;
+        }).haskell.packages.${ghc}.liquid-fixpoint;
 
         devShell = self.defaultPackage.${system}.env;
 
@@ -28,38 +33,41 @@
         ];
 
         overlays = {
-          patchHaskellGit = final: prev: haskellPackagesOverlay final prev (selfH: superH: {
-            git = prev.haskell.lib.overrideCabal (selfH.callHackage "git" "0.3.0" { }) (old: {
-              broken = false;
-              # git-0.3.0 defines a Monad a fail function, which is incompatible with ghc-8.10.1 https://hackage.haskell.org/package/git-0.3.0/docs/src/Data.Git.Monad.html#line-240
-              patches = [
-                (prev.writeText "git-0.3.0_fix-monad-fail-for-ghc-8.10.x.patch" ''
-                  diff --git a/Data/Git/Monad.hs b/Data/Git/Monad.hs
-                  index 480af9f..27c3b3e 100644
-                  --- a/Data/Git/Monad.hs
-                  +++ b/Data/Git/Monad.hs
-                  @@ -130 +130 @@ instance Resolvable Git.RefName where
-                  -class (Functor m, Applicative m, Monad m) => GitMonad m where
-                  +class (Functor m, Applicative m, Monad m, MonadFail m) => GitMonad m where
-                  @@ -242,0 +243 @@ instance Monad GitM where
-                  +instance MonadFail GitM where
-                  @@ -315,0 +317 @@ instance Monad CommitAccessM where
-                  +instance MonadFail CommitAccessM where
-                  @@ -476,0 +479 @@ instance Monad CommitM where
-                  +instance MonadFail CommitM where
-                '')
-              ];
+          patchHaskellGit = final: prev: haskellPackagesOverlay ghc final prev (selfH: superH:
+            with prev.haskell.lib; {
+              git = prev.haskell.lib.overrideCabal (selfH.callHackage "git" "0.3.0" { }) (old: {
+                broken = false;
+                # git-0.3.0 defines a Monad a fail function, which is incompatible with ghc-8.10.1 https://hackage.haskell.org/package/git-0.3.0/docs/src/Data.Git.Monad.html#line-240
+                patches = [
+                  (prev.writeText "git-0.3.0_fix-monad-fail-for-ghc-8.10.x.patch" ''
+                    diff --git a/Data/Git/Monad.hs b/Data/Git/Monad.hs
+                    index 480af9f..27c3b3e 100644
+                    --- a/Data/Git/Monad.hs
+                    +++ b/Data/Git/Monad.hs
+                    @@ -130 +130 @@ instance Resolvable Git.RefName where
+                    -class (Functor m, Applicative m, Monad m) => GitMonad m where
+                    +class (Functor m, Applicative m, Monad m, MonadFail m) => GitMonad m where
+                    @@ -242,0 +243 @@ instance Monad GitM where
+                    +instance MonadFail GitM where
+                    @@ -315,0 +317 @@ instance Monad CommitAccessM where
+                    +instance MonadFail CommitAccessM where
+                    @@ -476,0 +479 @@ instance Monad CommitM where
+                    +instance MonadFail CommitM where
+                  '')
+                ];
+              });
             });
-          });
-          addLiquidFixpoint = final: prev: haskellPackagesOverlay final prev (selfH: superH: {
-            liquid-fixpoint = prev.haskell.lib.overrideCabal (prev.haskellPackages.callCabal2nix "liquid-fixpoint" self { }) (old: {
-              buildTools = [ prev.z3 ];
-              doCheck = true;
-              doHaddock = true;
-              # bring the `fixpoint` binary into scope for tests run by nix-build
-              preCheck = ''export PATH="$PWD/dist/build/fixpoint:$PATH"'';
+          addLiquidFixpoint = final: prev: haskellPackagesOverlay ghc final prev (selfH: superH:
+            let callCabal2nix = prev.haskell.packages.${ghc}.callCabal2nix; in
+            with prev.haskell.lib; {
+              liquid-fixpoint = overrideCabal (callCabal2nix "liquid-fixpoint" self { }) (old: {
+                buildTools = [ prev.z3 ];
+                doCheck = true;
+                doHaddock = true;
+                # bring the `fixpoint` binary into scope for tests run by nix-build
+                preCheck = ''export PATH="$PWD/dist/build/fixpoint:$PATH"'';
+              });
             });
-          });
         };
 
       };
