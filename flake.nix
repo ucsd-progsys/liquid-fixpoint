b5b210101e23c790827de5d92cd150e83f6ef8f3
PLR <51248199+plredmond@users.noreply.github.com>
2021-03-11 22:46:48 -0800

    switch from haskellPackages.override to haskellPackages.extend

--- flake.nix
+++ flake.nix
@@ -26,44 +26,40 @@
 
         overlays = {
           patchHaskellGit = final: prev: {
-            haskellPackages = prev.haskellPackages.override {
-              overrides = selfH: superH: {
-                git = prev.haskell.lib.overrideCabal (selfH.callHackage "git" "0.3.0" { }) (old: {
-                  broken = false;
-                  # git-0.3.0 defines a Monad a fail function, which is incompatible with ghc-8.10.1 https://hackage.haskell.org/package/git-0.3.0/docs/src/Data.Git.Monad.html#line-240
-                  patches = [
-                    (prev.writeText "git-0.3.0_fix-monad-fail-for-ghc-8.10.x.patch" ''
-                      diff --git a/Data/Git/Monad.hs b/Data/Git/Monad.hs
-                      index 480af9f..27c3b3e 100644
-                      --- a/Data/Git/Monad.hs
-                      +++ b/Data/Git/Monad.hs
-                      @@ -130 +130 @@ instance Resolvable Git.RefName where
-                      -class (Functor m, Applicative m, Monad m) => GitMonad m where
-                      +class (Functor m, Applicative m, Monad m, MonadFail m) => GitMonad m where
-                      @@ -242,0 +243 @@ instance Monad GitM where
-                      +instance MonadFail GitM where
-                      @@ -315,0 +317 @@ instance Monad CommitAccessM where
-                      +instance MonadFail CommitAccessM where
-                      @@ -476,0 +479 @@ instance Monad CommitM where
-                      +instance MonadFail CommitM where
-                    '')
-                  ];
-                });
-              };
-            };
+            haskellPackages = prev.haskellPackages.extend (selfH: superH: {
+              git = prev.haskell.lib.overrideCabal (selfH.callHackage "git" "0.3.0" { }) (old: {
+                broken = false;
+                # git-0.3.0 defines a Monad a fail function, which is incompatible with ghc-8.10.1 https://hackage.haskell.org/package/git-0.3.0/docs/src/Data.Git.Monad.html#line-240
+                patches = [
+                  (prev.writeText "git-0.3.0_fix-monad-fail-for-ghc-8.10.x.patch" ''
+                    diff --git a/Data/Git/Monad.hs b/Data/Git/Monad.hs
+                    index 480af9f..27c3b3e 100644
+                    --- a/Data/Git/Monad.hs
+                    +++ b/Data/Git/Monad.hs
+                    @@ -130 +130 @@ instance Resolvable Git.RefName where
+                    -class (Functor m, Applicative m, Monad m) => GitMonad m where
+                    +class (Functor m, Applicative m, Monad m, MonadFail m) => GitMonad m where
+                    @@ -242,0 +243 @@ instance Monad GitM where
+                    +instance MonadFail GitM where
+                    @@ -315,0 +317 @@ instance Monad CommitAccessM where
+                    +instance MonadFail CommitAccessM where
+                    @@ -476,0 +479 @@ instance Monad CommitM where
+                    +instance MonadFail CommitM where
+                  '')
+                ];
+              });
+            });
           };
           addLiquidFixpoint = final: prev: {
-            haskellPackages = with prev.haskell.lib; prev.haskellPackages.override {
-              overrides = selfH: superH: {
-                liquid-fixpoint = overrideCabal (prev.haskellPackages.callCabal2nix "liquid-fixpoint" self { }) (old: {
-                  buildTools = [ prev.z3 ];
-                  doCheck = true;
-                  doHaddock = true;
-                  # bring the `fixpoint` binary into scope for tests run by nix-build
-                  preCheck = ''export PATH="$PWD/dist/build/fixpoint:$PATH"'';
-                });
-              };
-            };
+            haskellPackages = prev.haskellPackages.extend (selfH: superH: {
+              liquid-fixpoint = prev.haskell.lib.overrideCabal (prev.haskellPackages.callCabal2nix "liquid-fixpoint" self { }) (old: {
+                buildTools = [ prev.z3 ];
+                doCheck = true;
+                doHaddock = true;
+                # bring the `fixpoint` binary into scope for tests run by nix-build
+                preCheck = ''export PATH="$PWD/dist/build/fixpoint:$PATH"'';
+              });
+            });
           };
         };
 
