d827af0fa4251e21592812b978111257744b967c
Facundo Domínguez <facundo.dominguez@tweag.io>
2024-11-08 15:24:04 +0000

    Eliminate Monoid Reft instance

--- src/Language/Fixpoint/Types/Substitutions.hs
+++ src/Language/Fixpoint/Types/Substitutions.hs
@@ -179,13 +179,6 @@ disjoint (Su su) bs = S.null $ suSyms `S.intersection` bsSyms
     suSyms = S.fromList $ syms (M.elems su) ++ syms (M.keys su)
     bsSyms = S.fromList $ syms $ fst <$> bs
 
-instance Semigroup Reft where
-  (<>) = meetReft
-
-instance Monoid Reft where
-  mempty  = trueReft
-  mappend = (<>)
-
 meetReft :: Reft -> Reft -> Reft
 meetReft (Reft (v, ra)) (Reft (v', ra'))
   | v == v'          = Reft (v , pAnd [ra, ra'])
@@ -193,10 +186,10 @@ meetReft (Reft (v, ra)) (Reft (v', ra'))
   | otherwise        = Reft (v , pAnd [ra, ra' `subst1` (v', EVar v )])
 
 instance Semigroup SortedReft where
-  t1 <> t2 = RR (mappend (sr_sort t1) (sr_sort t2)) (mappend (sr_reft t1) (sr_reft t2))
+  t1 <> t2 = RR (mappend (sr_sort t1) (sr_sort t2)) (meetReft (sr_reft t1) (sr_reft t2))
 
 instance Monoid SortedReft where
-  mempty  = RR mempty mempty
+  mempty  = RR mempty trueReft
   mappend = (<>)
 
 instance Subable Reft where
