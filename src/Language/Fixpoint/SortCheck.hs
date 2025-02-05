8482e2806993ffb2726fe6de0d3c00617a9047ae
Vivien Rindisbacher <vrindisbacher77@gmail.com>
2025-02-04 21:09:30 -0800

    rip out some things

--- src/Language/Fixpoint/SortCheck.hs
+++ src/Language/Fixpoint/SortCheck.hs
@@ -552,10 +552,10 @@ elab f@(!_, !g) e@(EBin !o !e1 !e2) = do
 elab !f (EApp !e1 !e2) = do
   (!e1', !s1, !e2', !s2, !s) <- elabEApp f e1 e2
   let !e = eAppC s (eCst e1' s1) (eCst e2' s2)
-  ufRef <- asks ufM
-  uf <- liftIO $ readIORef ufRef
-  uf' <- unifyExprUF (snd f) uf e
-  liftIO $ atomicModifyIORef' ufRef $ const (uf', ())
+  -- ufRef <- asks ufM
+  -- uf <- liftIO $ readIORef ufRef
+  -- uf' <- unifyExprUF (snd f) uf e
+  -- liftIO $ atomicModifyIORef' ufRef $ const (uf', ())
   return (e, s)
 
 
@@ -1158,26 +1158,26 @@ checkEqConstrUF f e uf a t =
     Found tA -> unify1UF f e uf tA t
     _        -> throwErrorAt $ errUnifyMsg (Just "ceq2") e (FObj a) t
 
-{-# SCC unifyExprUF #-}
-unifyExprUF :: Env -> UF -> Expr -> CheckM UF
-unifyExprUF f uf (EApp e1 e2) = do
-  uf1 <- unifyExprUF f uf e1
-  uf2 <- unifyExprUF f uf1 e2
-  unifyExprAppUF f uf2 e1 e2
+-- {-# SCC unifyExprUF #-}
+-- unifyExprUF :: Env -> UF -> Expr -> CheckM UF
+-- unifyExprUF f uf (EApp e1 e2) = do
+--   uf1 <- unifyExprUF f uf e1
+--   uf2 <- unifyExprUF f uf1 e2
+--   unifyExprAppUF f uf2 e1 e2
 
-unifyExprUF f uf (ECst e _)
-  = unifyExprUF f uf e
-unifyExprUF _ uf _
-  = return uf
+-- unifyExprUF f uf (ECst e _)
+--   = unifyExprUF f uf e
+-- unifyExprUF _ uf _
+--   = return uf
 
-unifyExprAppUF :: Env -> UF -> Expr -> Expr -> CheckM UF
-unifyExprAppUF f uf e1 e2 = do
-  case (getArg $ exprSortMaybe e1, exprSortMaybe e2) of 
-    (Just s1, Just s2) -> unifyUF f uf (Just $ EApp e1 e2) s1 s2
-    _ -> return uf
-  where
-    getArg (Just (FFunc t1 _)) = Just t1
-    getArg _                   = Nothing
+-- unifyExprAppUF :: Env -> UF -> Expr -> Expr -> CheckM UF
+-- unifyExprAppUF f uf e1 e2 = do
+--   case (getArg $ exprSortMaybe e1, exprSortMaybe e2) of 
+--     (Just s1, Just s2) -> unifyUF f uf (Just $ EApp e1 e2) s1 s2
+--     _ -> return uf
+--   where
+--     getArg (Just (FFunc t1 _)) = Just t1
+--     getArg _                   = Nothing
 
 --------------------------------------------------------------------------------
 -- | Checking Predicates -------------------------------------------------------
--- src/Language/Fixpoint/Union.hs
+++ src/Language/Fixpoint/Union.hs
@@ -4,14 +4,6 @@ import Data.HashMap.Strict (lookup, insert, HashMap, empty)
 import Prelude hiding (lookup)
 import Language.Fixpoint.Types (Sort(..))
 
-unionSub :: UF -> Sort -> Sort -> UF
-unionSub uf s1 s2 = 
-    -- get the parents and call unionVals 
-    let s1_root = getRep uf s1
-        s2_root = getRep uf s2 
-    in 
-        unionVals uf s1_root s2_root
-
 --------------------------------------------------------------------------------
 -- | union for sorts in union find
 --------------------------------------------------------------------------------
@@ -28,12 +20,14 @@ unionVals uf s1 s2
 
 unionVals uf (FObj x) (FObj y)
     | x == y = uf
--- unionVals u@(MkUF uf) (FVar i) (FVar j) = if i == j then u else MkUF (insert i (FVar j) uf)
+unionVals u@(MkUF uf) (FVar i) (FVar j) = if i == j then u else MkUF (insert i (FVar j) uf)
 unionVals (MkUF uf) (FVar i) s = MkUF (insert i s uf)
 unionVals (MkUF uf) s (FVar i) = MkUF (insert i s uf)
-unionVals uf (FFunc s1 s2) (FFunc s1' s2') = let uf' = unionSub uf s1 s1' in unionSub uf' s2 s2'
-unionVals uf (FApp s1 s2) (FApp s1' s2') = let uf' = unionSub uf s1 s1' in unionSub uf' s2 s2'
-unionVals uf (FAbs _ s) (FAbs _ s') = unionSub uf s s'
+unionVals uf (FFunc s1 s2) (FFunc s1' s2') = unionVals uf' (getRep uf' s2) (getRep uf' s2')
+    where uf' = unionVals uf (getRep uf s1) (getRep uf s1')
+unionVals uf (FApp s1 s2) (FApp s1' s2') = unionVals uf' (getRep uf' s2) (getRep uf' s2')
+    where uf' = unionVals uf (getRep uf s1) (getRep uf s1')
+unionVals uf (FAbs _ s) (FAbs _ s') = unionVals uf (getRep uf s) (getRep uf s')
 unionVals uf (FTC s1) (FTC s2) | s1 == s2 = uf
 unionVals _ s1 s2 = error ("Cannot unify " ++ show s1 ++ " and " ++ show s2)
 
