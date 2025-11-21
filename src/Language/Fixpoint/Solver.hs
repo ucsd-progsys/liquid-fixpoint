95dbfa6328bd370ea86c5b8ef239585979d348fd
Facundo Domínguez <facundo.dominguez@tweag.io>
2025-11-21 10:08:36 +0000

    Fix recursion in simplifyKVar

--- src/Language/Fixpoint/Solver.hs
+++ src/Language/Fixpoint/Solver.hs
@@ -366,7 +366,7 @@ simplifyKVar = go
     go (PAnd es) = PAnd $ map go es
     go (PExist bs0 (PExist bs1 p)) =
       let bs0' = filter (\(x,_) -> x `notElem` map fst bs1) bs0
-       in PExist (bs0' ++ bs1) p
+       in go (PExist (bs0' ++ bs1) p)
     go (PExist bs e0) =
       let es = map go (conjuncts e0)
           esv = map (isVarEq (map fst bs)) es
