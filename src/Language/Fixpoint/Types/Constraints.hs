c0b72fe654ad1ff683051b79fdd42d85e7cea0d4
Facundo Domínguez <facundo.dominguez@tweag.io>
2025-11-24 18:23:38 +0000

    Use braces when pretty printing of qualifiers

--- src/Language/Fixpoint/Types/Constraints.hs
+++ src/Language/Fixpoint/Types/Constraints.hs
@@ -564,11 +564,11 @@ instance (Ord v, Fixpoint v, PPrint v) => PPrint (QualifierV v) where
   pprintTidy k q =
     "qualif" <+> pprintTidy k (qName q) <+>
      parens (hsep $ punctuate comma (pprintTidy k <$> qParams q)) <+>
-     colon <+> pprintTidy k (qBody q) <+> "//defined at" <+> pprintTidy k (qPos q)
+     braces (pprintTidy k (qBody q)) <+> "//defined at" <+> pprintTidy k (qPos q)
 
 
 pprQual :: Qualifier -> Doc
-pprQual (Q n xts p l) = text "qualif" <+> text (symbolString n) <-> parens args <-> colon <+> parens (toFix p) <+> text "//" <+> toFix l
+pprQual (Q n xts p l) = text "qualif" <+> text (symbolString n) <-> parens args <-> braces (toFix p) <+> text "//" <+> toFix l
   where
     args              = intersperse comma (toFix <$> xts)
 
