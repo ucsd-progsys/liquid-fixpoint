4089241b57b4529aa36acd5584c3f55f6ce9750d
Nico Lehmann <nico.lehmannm@gmail.com>
2025-12-23 11:56:45 -0300

    Remove noSmtHorn flag and Fixpoint.Horn.Parse

--- src/Language/Fixpoint/Horn/Solve.hs
+++ src/Language/Fixpoint/Horn/Solve.hs
@@ -16,7 +16,6 @@ import qualified Language.Fixpoint.Types        as F
 import qualified Language.Fixpoint.Types.Config as F
 import qualified Language.Fixpoint.Horn.Types   as H
 
-import qualified Language.Fixpoint.Horn.Parse   as H
 import qualified Language.Fixpoint.Horn.SMTParse   as SH
 
 import qualified Language.Fixpoint.Horn.Transformations as Tx
@@ -47,13 +46,12 @@ solveHorn baseCfg0 = do
 
 parseQuery :: F.Config -> IO H.TagQuery
 parseQuery cfg
-  | F.stdin cfg = Parse.parseFromStdIn hornP
+  | F.stdin cfg = Parse.parseFromStdIn SH.hornP
   | json        = loadFromJSON file
-  | otherwise   = Parse.parseFromFile hornP file
+  | otherwise   = Parse.parseFromFile SH.hornP file
   where
     json  = Files.isExtFile Files.Json file
     file  = F.srcFile cfg
-    hornP = if F.noSmtHorn cfg then H.hornP else SH.hornP
 
 loadFromJSON :: FilePath -> IO H.TagQuery
 loadFromJSON f = do
