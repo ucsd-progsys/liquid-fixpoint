8eb0faf8a0f911fecf87ec9fc503b295fdf752cd
Ranjit Jhala <rjhala@ucsd.edu>
2026-03-17 12:04:49 -0700

    tidy

--- src/Language/Fixpoint/Smt/Serialize.hs
+++ src/Language/Fixpoint/Smt/Serialize.hs
@@ -6,7 +6,6 @@
 {-# LANGUAGE DoAndIfThenElse      #-}
 
 {-# OPTIONS_GHC -Wno-orphans        #-}
-{-# LANGUAGE InstanceSigs #-}
 
 -- | This module contains the code for serializing Haskell values
 --   into SMTLIB2 format, that is, the instances for the @SMTLIB2@
@@ -128,7 +127,6 @@ instance SMTLIB2 LocSymbol where
   smt2 = smt2 . val
 
 instance SMTLIB2 SymConst where
-  smt2 :: SymConst -> SymM Builder
   smt2 c@(SL t) = do
     seStr <- gets seString
     if seStr
